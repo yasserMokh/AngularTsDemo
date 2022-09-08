9c2a2dd550637ee2af8378e69d9911cf55156d64
yasserMokh <yasser.moukhtar@outlook.com>
2022-09-08 08:07:58 +0200

    - Angular Routing

--- AngularTsDemo/src/app/app.module.ts
+++ AngularTsDemo/src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './components/employee/employee.component';
 import { EmployeeListComponent } from './components/employee/employee.list.component';
 
-import { GenderFilterComponent } from './filters/gender.filter.component';
+import { GenderFilterComponent } from './components/filters/gender.filter.component';
 
 import { GenderTitlePipe } from './pipes/genderTitle.pipe';
 
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/error/notFound.component';
+import { AppMenuComponent } from './components/menu/appMenu.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+]
+
 @NgModule({
   declarations: [
-    AppComponent, EmployeeComponent, EmployeeListComponent, GenderFilterComponent, GenderTitlePipe
+    AppComponent, EmployeeComponent, EmployeeListComponent, GenderFilterComponent, GenderTitlePipe, HomeComponent, NotFoundComponent, AppMenuComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, FormsModule
+    BrowserModule, HttpClientModule, FormsModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AngularTsDemo/src/app/components/menu/appMenu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-menu',
+  templateUrl:'./appMenu.component.html'
+})
+export class AppMenuComponent {
+
+}
